c2d08a4ffedafff02a222897ed56dda04cfc07e6
programMike <programMike@github.com>
2018-01-28 11:34:56 -0800

    Grant Types Assignment
    Modified files of the project for the Grant Types display from model assignment

--- CommAsst17in18for172/Controllers/HomeController.cs
+++ CommAsst17in18for172/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+//2 * Add a using reference to the project models
+using CommAsst17in18for172.Models;
+
+
+//1 * Find the home controller:
+//i found it!  programming god 2 b
 
 namespace CommAsst17in18for172.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
-            return View();
+        {            
+            CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+            //3 * Modify the index method to create a list of objects
+            //4 * Have that list returned to the view        
+            return View(db.GrantTypes.ToList());
         }
 
         public ActionResult About()

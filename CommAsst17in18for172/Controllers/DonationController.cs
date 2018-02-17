c872c12535a27845f34bfbf666bb11ba238307b3
programMike <programMike@github.com>
2018-02-17 14:02:09 -0800

    Donate to Community Assist
    Donate to Community Assist, Assignment 6

---
+++ CommAsst17in18for172/Controllers/DonationController.cs
@@ -0,0 +1,65 @@
+﻿/*Donate, Assignment 6
+ *Add a controller to handle donations.*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommAsst17in18for172.Models;
+
+namespace CommAsst17in18for172.Controllers
+{
+    public class DonationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+        Message argMessage = new Message();
+        // GET: Donation
+        public ActionResult Index()
+        {
+            /*Donate, Assignment 6
+             *You should check to make sure the user is logged in.
+             *You can do this by checking to see if the Session variable is
+             *null or not. Null means they are not logged in.*/
+            if (Session["sessClientKey"] == null)
+            {
+                argMessage.MessageText = "You must be authenticated to " +
+                    "donate to the Community Assist Project";
+                /*Donate, Assignment 6
+                 *If they are not logged in, you should redirect them to a
+                 *result page where it tells them they must be logged in*/
+                return RedirectToAction("Result", argMessage);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Amount")]Donate donate)
+        {
+            /*Donate, Assignment 6
+             *if they are logged in, write the amount and the personkey and
+             *the date and set the guid for a new Donation Class*/
+            Donation donation = new Donation();
+            donation.DonationAmount = donate.Amount;
+            donation.PersonKey = Session["sessClientKey"].GetHashCode();
+            donation.DonationDate = DateTime.Now;
+            donation.DonationConfirmationCode = Guid.NewGuid();
+
+            db.Donations.Add(donation);
+            /*Donate, Assignment 6
+             *Save the changes to the database.*/
+            db.SaveChanges();
+
+            /*Donate, Assignment 6
+             *The result page should now thank them for their donation.*/
+            argMessage.MessageText = "Thank for your donation to the " +
+                "Community Assist Project";
+            return View("Result", argMessage);
+        }
+
+        public ActionResult Result(Message paraMessage)
+        {
+            return View(paraMessage);
+        }
+    }
+}
---
+++ CommAsst17in18for172/Models/Donate.cs
@@ -0,0 +1,17 @@
+﻿/*Donate, Assignment 6
+ *... (again you may need to make a donation class, in this case probably
+ *not containing much other than the amount)*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommAsst17in18for172.Models
+{
+    public class Donate
+    {
+        /*define this class member data type to match the auto genterated
+         *DonationAmount decimal data type of the Models~Donation class*/
+        public decimal Amount { set; get; }
+    }
+}
